f78c7c9e000bf8dd25fda29f0bdf03443bd1feff
Jonathan Mutakura <jmutakura@gmail.com>
2020-02-22 02:42:18 -0600

    Current progress

--- src/catchPokemon.java
+++ src/catchPokemon.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class catchPokemon {
 
+
+    // Driver Code
     public static void main(String[] args) {
-        String[] caughtPokemon = addPokemon();
-        System.out.println(Arrays.toString(caughtPokemon));
+
 
     }
 
+    // Method for catching the Pokemon
     static String[] addPokemon(){
         Scanner getData = new Scanner(System.in);
         String[] caughtPokemon = new String[10];
@@ -18,7 +20,7 @@ public class catchPokemon {
             System.out.println("Which Pokemon have you caught?");
             String catching = getData.nextLine();
             caughtPokemon[count] = catching;
-            System.out.println("Would you still like to continue catching pokemon?");
+            System.out.println("Would you still like to continue catching pokemon? (y / n)");
             continueCatching = getData.nextLine();
             System.out.println("So far you have caught " + caughtPokemon[count]);
             count += 1;
@@ -27,5 +29,17 @@ public class catchPokemon {
         System.out.println("Time to head to the Pokemon Gym...");
 
         return caughtPokemon;
+    }
+
+    // Method for taking the Pokemon to the gym team
+    static String[] addGymTeam(){
+        Scanner getData = new Scanner(System.in);
+        String[] collection = addPokemon();
+        String[] gymTeam = new String[6];
+
+        while (gymTeam.length < 6){
+            System.out.println("Which Pokemon would you like to add to your F=Gym team?");
+            String gymP = getData.nextLine();
+        }
     }
 }
